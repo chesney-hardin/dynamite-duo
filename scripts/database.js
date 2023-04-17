6cf6379b2b289af4a20cf25d82cfa8e5606e12ed
Chesney Hardin <chesney.hardin@gmail.com>
2023-04-17 15:58:29 -0500

    adding villains and heros scripts

---
+++ scripts/database.js
@@ -0,0 +1,36 @@
+const database = {
+    heroes: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+    ]
+
+    villains: [
+        {
+            id: 1,
+            name: "Doomdog",
+            power: "Initiates avalanches"
+        },
+        {
+            id: 2,
+            name: "Triptime",
+            power: "Makes time disappear"
+        }
+    ]
+}
+
+
+export const getHeroes = () => {
+    return database.heroes.map(hero => ({...hero}))
+}
+
+export const getVillains = () => {
+    return database.villains.map(villain => ({...villain}))
+}
